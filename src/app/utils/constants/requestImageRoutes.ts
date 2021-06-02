60ecdc9bfc1b063e661afd8e5ca5f78db4e3f42c
lsandoval9 <lizarraga309@gmail.com>
2021-06-02 06:20:34 -0400

    fixed an error with the production env

--- src/app/utils/constants/requestImageRoutes.ts
+++ src/app/utils/constants/requestImageRoutes.ts
@@ -2,7 +2,7 @@ import { environment } from "src/environments/environment";
 
 
 export const API_ROUTES = {
-    HOST: environment.host /* "http://localhost:8080/" */ /* "https://spring-media.herokuapp.com/" */,
+    HOST: "" /* "http://localhost:8080/" */ /* "https://spring-media.herokuapp.com/" */,
     IMAGE_FILTER: "im/",
     ASCII_FILTER: "ascii/",
     DETECT: "detect/",
